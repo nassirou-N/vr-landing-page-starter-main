cd2b0e3f6ecd48e85a85b7829f08f418d5cec37b
nassirou <nassirou9993@gmail.com>
2023-08-24 13:20:30 +0100

    the experience section

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import React, { useState } from 'react';
 import Banner from './components/Banner';
 import Header from './components/Header';
 import NavMobile from './components/NavMobile';
+import Experience from './components/Experience';
 
 const App = () => {
   const [navMob, setNavMob] = useState(false);
   const handelMobile = () => {
     setNavMob(!navMob);
   }
-  return <>
+  return <div className='relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex '>
     <Header handelMobile = {handelMobile} navMob={navMob} />
     <Banner />
     {/**mobile nav */}
     <div className={`${navMob ? 'right-0':'-right-full'} fixed top-[75px] lg:top-[94px] w-[35%] bottom-0 transition-all`}>
       <NavMobile />
     </div>
-  </>;
+    {/**experience */}
+    <Experience />
+  </div>;
 };
 
 export default App;
