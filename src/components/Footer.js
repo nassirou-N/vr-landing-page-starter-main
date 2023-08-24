bff570b3fa9da6b225b072b6099fdc14fd4e8031
nassirou <nassirou9993@gmail.com>
2023-08-24 17:34:16 +0100

    the footer VR Nassirou

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=''>
+        <div className='container mx-auto flex items-center justify-center pb-3'>
+            <div className=''>copyright by  ✍  <span className='bg-gradient-to-r from-[#b936f5] to-[#f1006c] text-transparent bg-clip-text font-semibold text-xl'>Nassirou</span></div>
+        </div>
+      
+    </footer>
+  )
+}
+
+export default Footer
